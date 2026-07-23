811590ab322f27a7e0b9dcc9784f1df5cd3d3cab
chfbledsoe <ebledsoe@lakejunaluska.com>
2026-07-23 14:51:09 -0400

    Set Permissions-Policy header in response
    Modify response headers to set Permissions-Policy for camera and microphone.

--- src/index.js
+++ src/index.js
@@ -36,6 +36,9 @@ export default {
     }
 
     // --- Everything else: serve the static app files as before ---
-    return env.ASSETS.fetch(request);
+    const response = await env.ASSETS.fetch(request);
+    const newResponse = new Response(response.body, response);
+    newResponse.headers.set('Permissions-Policy', 'camera=(self), microphone=()');
+    return newResponse;
   }
 };
